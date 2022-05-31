d4f8fd60543077f57195a6c15f2ed649687fa473
Lin <hszl000@163.com>
2022-06-01 00:34:43 +0800

    feat(初始化项目（vite+vue3.2）): 集成 element-plus、Prettier 代码格式化工具、git cz 提交约束

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    vue3
+    <el-button @click="show">林大树</el-button>
+  </div>
+</template>
+
+<script setup>
+import { ElMessage } from "element-plus";
+
+const show = () => {
+  ElMessage.success("欢迎");
+};
+</script>
+
+<style lang="scss" scoped></style>
