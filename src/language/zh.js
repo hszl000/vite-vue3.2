d1b7e29370e004b31c63d91b312e13fbe6dd97e5
Lin <hszl000@163.com>
2022-09-04 23:21:52 +0800

    feat(新增国际化功能,集成scss,完成login页面布局): 新增国际化功能,集成scss,完成login页面布局

---
+++ src/language/zh.js
@@ -0,0 +1,9 @@
+export default {
+  login: {
+    reset: '重置',
+    submit: '登录',
+    userName: '用户名',
+    passWord: '密码',
+    placeholder: '请输入'
+  }
+}
---
+++ src/widgets/index.js
@@ -0,0 +1,7 @@
+import useLocaleInject from '@/widgets/useLocaleInject.js'
+
+export default {
+  install(Vue) {
+    Vue.use(useLocaleInject)
+  }
+}
